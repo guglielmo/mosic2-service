a3f399c7610dea0a90b09b7a557e93bd25e8baff
guglielmo <guglielmo.celata@gmail.com>
2017-05-18 16:18:27 +0200

    PATH_ALLEGATI_REGISTRI changed

--- src/UserBundle/Entity/Costanti.php
+++ src/UserBundle/Entity/Costanti.php
@@ -4,7 +4,7 @@ namespace UserBundle\Entity;
 
 class Costanti {
 
-    const PATH_ALLEGATI_REGISTRI = "/var/www/clients/client1/web11/web/web/files/REGISTRO_MOSIC/";
+    const PATH_ALLEGATI_REGISTRI = "/var/www/mosic2/service/web/files/REGISTRO_MOSIC";
     const URL_ALLEGATI_REGISTRI = "files/REGISTRO_MOSIC/";
     const URL_ALLEGATI_PRECIPE = "files/RIUNIONI_PRECIPE/";
     const URL_ALLEGATI_DELIBERE = "files/DELIBERE/";
