67ebda1689ba9faf4c6d24606675f899ebc12859
guglielmo <guglielmo.celata@gmail.com>
2017-11-13 10:57:02 +0100

    path changed in costanti

--- src/UserBundle/Entity/Costanti.php
+++ src/UserBundle/Entity/Costanti.php
@@ -4,13 +4,13 @@ namespace UserBundle\Entity;
 
 class Costanti {
 
-    //const PATH_IN_DATABASE = "service/";
-    const PATH_IN_DATABASE = "";
+    const PATH_IN_DATABASE = "service/";
+    //const PATH_IN_DATABASE = "";
 
     const PATH_IN_SERVER = "";
 
-    //const PATH_PHP = "/usr/bin/php";
-    const PATH_PHP = "/opt/php-5.6.25/bin/php";
+    const PATH_PHP = "/usr/bin/php";
+    // const PATH_PHP = "/opt/php-5.6.25/bin/php";
 
     const URL_ALLEGATI_REGISTRI = Costanti::PATH_IN_DATABASE . "files/REGISTRO_MOSIC";
     const URL_ALLEGATI_PRECIPE = Costanti::PATH_IN_DATABASE . "files/RIUNIONI_PRECIPE";
